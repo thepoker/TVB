b025ae845b6e1ec9fb9cb9a9f4769d030083d569
Bryan Pope <bryanp@rental-network.com>
2012-12-19 23:32:11 -0500

    Modified misc. project files

---
+++ SilverlightHotelBrowser/SilverlightHotelBrowser.Web/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SilverlightHotelBrowser.Web
+{
+    public class Class1
+    {
+    }
+}
--- SilverlightHotelBrowser/SilverlightHotelBrowser.Web/Hotel.cs
+++ SilverlightHotelBrowser/SilverlightHotelBrowser.Web/Hotel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//BP:12/19/2012 - Test
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
